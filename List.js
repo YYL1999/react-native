84950aafb5031e821f6a92689dbfc29f68384fa1
YYL1999 <2291003927@qq.com>
2019-03-10 16:57:05 +0800

    bug

--- List.js
+++ List.js
@@ -116,61 +116,7 @@ export default class List extends Component{
         }, this.props.duration);
  
     }
-    _hasMore(){
-       return cacheResults.items.length!==cacheResults.total
-    }
-    _fetchMoreData(){
-        if(!(cacheResults.items.length!==cacheResults.total) || this.state.isLoadingTail==true ){
-            return
-        }
-       const page=cacheResults.nextPage
-       this.fetchData(page)
-    }
-    renderRow(row){
-      return(
-        <TouchableHighlight>
-        <View style={styles.item}>
-            <Text style={styles.title}>{row.id}</Text>
-            <Image
-              source={{url:row.thumb}}
-              style={styles.thumb}
-              >
-              {/* <Icon 
-                name='ios-play'
-                size={28}
-                style={styles.play} /> */}
-            </Image>
-            <View style={styles.itemFooter} >
-              <View style={styles.handleBox}>
-                {/* <Icon
-                 // name='ios-heart-outline'
-                  size={28}
-                  style={styles.up} /> */}
-                  <Text style={styles.handleText}>
-                      喜欢
-                  </Text>
-              </View>
-              <View style={styles.handleBox}>
-                {/* <Icon
-                  //name='ios-chatbubble-outline'
-                  size={28}
-                  style={styles.commentIcon} /> */}
-                  <Text style={styles.handleText}>
-                      评论
-                  </Text>
-              </View>
-            </View>
-        </View>
-    </TouchableHighlight>
-      )
-    }
-
-    componentDidMount(){
-            console.log("aaa")
-             this.fetchData(1)
-    }
     render(){
-<<<<<<< HEAD
         let array = [{ content: '' }];
         if (dataArray && dataArray.length > 0) {
             array = [];
@@ -278,24 +224,6 @@ export default class List extends Component{
          </Text>
            </View>
        </View>
-=======
-        console.log('bbb')
-      return(
-        <View style={styles.childViewStyle}>
-        <View style={styles.header}>
-           <Text style={styles.headerTitle}>
-            列表页面
-           </Text>
-        </View>
-        <FlatList
-          data={this.state.dataSource}//关联state中的datasource
-          renderItem={({item}) => (
-           this.renderRow(item)
-          )}
-          onEndReached={this._fetchMoreData}
-          onEndReachedThreshold={20}
-          />
->>>>>>> 5a5728325591eeaa03c98d56f26f1fae67cf05c9
       </View>
       )
     }
@@ -373,11 +301,10 @@ const styles=StyleSheet.create({
     childViewStyle:{
         height:'100%',
         width:'100%',
-         flex: 1,
-        //  justifyContent: 'center',
+        flex: 1,
+         justifyContent: 'center',
          alignItems: 'center',
        },
-<<<<<<< HEAD
        scrollStyle:{
         margin:10,
         height:150,
@@ -477,73 +404,4 @@ fontstyle:{
    position:'absolute',
 
 }
-=======
-       header:{
-         paddingTop:35,
-         paddingBottom:12,
-         backgroundColor:'#ee735c',
-         width:'100%'
-       },
-       headerTitle:{
-           color:'#fff',
-           fontSize:16,
-           textAlign:'center',
-           fontWeight:'600'
-       },
-       item:{
-           width:width,
-           marginBottom:10,
-           backgroundColor:'#fff'
-       },
-       thumb:{
-           width:width,
-           height:width*0.5,
-           resizeMode:'cover'
-       },
-       title:{
-           padding:10,
-           fontSize:18,
-           color:'#333'
-       },
-       itemFooter:{
-           flexDirection:'row',
-           justifyContent:'space-between',
-           backgroundColor:'#eee'
-       },
-       handleBox:{
-           padding:10,
-           flexDirection:'row',
-           width:width/2-0.5,
-           justifyContent:'center',
-           backgroundColor:'#fff'
-       },
-       play:{
-           position:"absolute",
-           bottom:14,
-           right:14,
-           width:46,
-           height:46,
-           paddingTop:9,
-           paddingLeft:18,
-           backgroundColor:'transparent',
-           borderColor:'#fff',
-           borderWidth:1,
-           borderRadius:23,
-           color:'#ed7b66'
-       },
-       handleText:{
-           paddingLeft:12,
-           fontSize:18,
-           color:'#333'
-       },
-       up:{
-           fontSize:22,
-           color:'#333'
-       },
-       commentIcon:{
-           fontSize:22,
-           color:'#333'
-       }
->>>>>>> 5a5728325591eeaa03c98d56f26f1fae67cf05c9
 })
-export default List
